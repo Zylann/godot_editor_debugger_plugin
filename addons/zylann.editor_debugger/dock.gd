dc8e24b3d249f49b87609efffc5dd2aec4181c2d
Xananax <me@xananax.com>
2023-01-21 13:15:49 +0100

    Replace button with 3 popup actions

--- addons/zylann.editor_debugger/dock.gd
+++ addons/zylann.editor_debugger/dock.gd
@@ -6,13 +6,24 @@ const Util = preload("util.gd")
 signal node_selected(node)
 
 @onready var _popup_menu: PopupMenu = get_node("PopupMenu")
-@onready var _save_branch_as_scene_button: Button = get_node("PopupMenu/SaveBranchAsSceneButton")
 @onready var _inspection_checkbox: CheckBox = get_node("VBoxContainer/ShowInInspectorCheckbox")
 @onready var _label: Label = get_node("VBoxContainer/Label")
 @onready var _tree_view: Tree = get_node("VBoxContainer/Tree")
 @onready var _save_branch_file_dialog: FileDialog = get_node("SaveBranchFileDialog")
 
 const METADATA_NODE_NAME = 0
+
+enum POPUP_ACTIONS {
+	SAVE_BRANCH_AS_SCENE,
+	COPY_PATH_TO_CLIPBOARD,
+	COPY_NODE_TYPES_TO_CLIPBOARD
+}
+
+const popup_actions = {
+	POPUP_ACTIONS.SAVE_BRANCH_AS_SCENE: "Save branch as scene",
+	POPUP_ACTIONS.COPY_PATH_TO_CLIPBOARD: "Copy path to clipboard",
+	POPUP_ACTIONS.COPY_NODE_TYPES_TO_CLIPBOARD: "Copy node types to clipboard",
+}
 
 var _update_interval := 1.0
 var _time_before_next_update := 0.0
@@ -26,6 +37,11 @@ var _no_texture := get_theme_icon("", "EditorIcons")
 
 func get_tree_view() -> Tree:
 	return _tree_view
+
+
+func _ready() -> void:
+	for id in popup_actions:
+		_popup_menu.add_item(popup_actions[id], id)
 
 
 func _enter_tree() -> void:
@@ -325,10 +341,28 @@ func _on_ShowInInspectorCheckbox_toggled(_button_pressed: bool) -> void:
 	pass
 
 
-func _on_SaveBranchAsSceneButton_pressed() -> void:
-	#_save_branch_as_scene_button.accept_event()
+func _on_popup_menu_index_pressed(index: int) -> void:
 	_popup_menu.hide()
-	_save_branch_file_dialog.popup_centered_ratio()
+	match index:
+		POPUP_ACTIONS.SAVE_BRANCH_AS_SCENE:
+			_save_branch_file_dialog.popup_centered_ratio()
+		POPUP_ACTIONS.COPY_PATH_TO_CLIPBOARD:
+			var node_view := _tree_view.get_selected()
+			var node := _get_node_from_view(node_view)
+			DisplayServer.clipboard_set(node.get_path())
+			print("Copied to clipboard: %s"%[node.get_path()])
+		POPUP_ACTIONS.COPY_NODE_TYPES_TO_CLIPBOARD:
+			var node_view := _tree_view.get_selected()
+			var node := _get_node_from_view(node_view)
+			var node_types := []
+			while node.get_parent():
+				var tuple := PackedStringArray([node.get_class(), node.name])
+				node_types.append(tuple)
+				node = node.get_parent()
+			node_types.reverse()
+			var node_types_str := "%s"%[node_types]
+			DisplayServer.clipboard_set(node_types_str)
+			print("Copied to clipboard: %s"%[node_types_str])
 
 
 func _on_SaveBranchFileDialog_file_selected(path: String) -> void:
@@ -343,4 +377,5 @@ func _on_SaveBranchFileDialog_file_selected(path: String) -> void:
 	ResourceSaver.save(packed_scene, path)
 	# Revert ownership of all children.
 	restore_ownership(node, owners, true)
+
 
